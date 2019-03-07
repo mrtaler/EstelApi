4c95a429ae459aff303b5e872ec57775802bc67f
Siarhei Linkevich <Siarhei_Linkevich@epam.com>
2019-03-08 01:52:20 +0300

    Refactoring

---
+++ src/Application/EstelApi.Application/ApplicationCqrs/Commands/CustomerCommands/Commands/CustomerCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EstelApi.Application.Cqrs.Commands.Base;
+using EstelApi.Core.Seedwork.CoreCqrs.Commands;
+using EstelApi.Domain.DataAccessLayer.Context.CoreEntities.CustomerAgg;
+using MediatR;
+
+namespace EstelApi.Application.Cqrs.Commands.Commands.CustomerCommands.Commands
+{
+    public abstract class CustomerCommand : Customer,ICommand, IValidated, IRequest<CommandResponse<Customer>>
+    {
+        public bool AlreadyValidated { get; set; }
+    }
+}
--- src/Application/EstelApi.Application/Services/CustomerAppService.cs
+++ src/Application/EstelApi.Application/Services/CustomerAppService.cs
@@ -16,6 +16,7 @@ using MediatR;
 
 namespace EstelApi.Application.Services
 {
+    using EstelApi.Application.Dto;
     using EstelApi.Domain.DataAccessLayer.Context.CoreEntities.CustomerAgg;
 
     public class CustomerAppService : ICustomerAppService
@@ -35,12 +36,12 @@ namespace EstelApi.Application.Services
             this.bus = bus;
             this.eventStoreRepository = eventStoreRepository;
         }
-
+        #region 2
         public IEnumerable<CustomerViewModelApp> GetAll()
         {
             var result = this.bus.Send(new AllCustomersQuery()).Result;
 
-            var ret = result.ProjectedAsCollection<CustomerViewModelApp>();
+            List<CustomerViewModelApp> ret = result.ProjectedAsCollection<CustomerViewModelApp>();
 
             return
                 ret; // _customerRepository.GetAll().AsQueryable().ProjectTo<CustomerViewModelApp>(_mapper.ConfigurationProvider);
@@ -55,7 +56,7 @@ namespace EstelApi.Application.Services
         {
             var registerCommand = customerViewModel.ProjectedAs<RegisterNewCustomerCommand>();// _mapper.Map<>(customerViewModel);
             var result = await this.bus.Send(registerCommand);
-            if ( result .IsSuccess)
+            if (result.IsSuccess)
             {
                 var retVal = new CustomerViewModelApp
                 {
@@ -70,7 +71,6 @@ namespace EstelApi.Application.Services
 
             return new CustomerViewModelApp();
         }
-
 
         public void Update(UpdateCustomerViewModel customerViewModel)
         {
@@ -92,6 +92,157 @@ namespace EstelApi.Application.Services
         public void Dispose()
         {
             GC.SuppressFinalize(this);
+        }
+
+        #endregion
+        public CustomerDTO AddNewCustomer(CustomerDTO customerDTO)
+        {
+            //check preconditions
+            if (customerDTO == null || customerDTO.CountryId == Guid.Empty)
+                throw new ArgumentException("_resources.GetStringResource(LocalizationKeys.Application.warning_CannotAddCustomerWithEmptyInformation)");
+// to command handler
+            var country = _countryRepository.Get(customerDTO.CountryId);
+
+            if (country != null)
+            {
+                //Create the entity and the required associated data
+                var address = new Address(
+                    customerDTO.AddressCity, 
+                    customerDTO.AddressZipCode,
+                    customerDTO.AddressAddressLine1, 
+                    customerDTO.AddressAddressLine2);
+
+                var customer = CustomerFactory.CreateCustomer(customerDTO.FirstName,
+                                                              customerDTO.LastName,
+                                                              customerDTO.Telephone,
+                                                              customerDTO.Company,
+                                                              country,
+                                                              address);
+
+                //save entity
+                SaveCustomer(customer);
+
+                //return the data with id and assigned default values
+                return customer.ProjectedAs<CustomerDTO>();
+            }
+            else
+                return null;
+        }
+
+        public void UpdateCustomer(CustomerDTO customerDTO)
+        {
+            if (customerDTO == null || customerDTO.Id == Guid.Empty)
+                throw new ArgumentException(_resources.GetStringResource(LocalizationKeys.Application.warning_CannotUpdateCustomerWithEmptyInformation));
+
+            //get persisted item
+            var persisted = _customerRepository.Get(customerDTO.Id);
+
+            if (persisted != null) //if customer exist
+            {
+                //materialize from customer dto
+                var current = MaterializeCustomerFromDto(customerDTO);
+
+                //Merge changes
+                _customerRepository.Merge(persisted, current);
+
+                //commit unit of work
+                _customerRepository.UnitOfWork.Commit();
+            }
+            else
+                _logger.LogWarning(_resources.GetStringResource(LocalizationKeys.Application.warning_CannotUpdateNonExistingCustomer));
+        }
+
+        public void RemoveCustomer(Guid customerId)
+        {
+            var customer = _customerRepository.Get(customerId);
+
+            if (customer != null) //if customer exist
+            {
+                //disable customer ( "logical delete" ) 
+                customer.Disable();
+
+                //commit changes
+                _customerRepository.UnitOfWork.Commit();
+            }
+            else //the customer not exist, cannot remove
+                _logger.LogWarning(_resources.GetStringResource(LocalizationKeys.Application.warning_CannotRemoveNonExistingCustomer));
+        }
+
+        public List<CustomerListDTO> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerListDTO> FindCustomers(string text)
+        {
+            //get the specification
+
+            var enabledCustomers = CustomerSpecifications.EnabledCustomers();
+            var filter = CustomerSpecifications.CustomerFullText(text);
+
+            ISpecification<Customer> spec = enabledCustomers & filter;
+
+            //Query this criteria
+            var customers = _customerRepository.AllMatching(spec);
+
+            if (customers != null
+                &&
+                customers.Any())
+            {
+                //return adapted data
+                return customers.ProjectedAsCollection<CustomerListDTO>();
+            }
+            else // no data..
+                return null;
+        }
+
+        public CustomerDTO FindCustomer(Guid customerId)
+        {
+            //recover existing customer and map
+            var customer = _customerRepository.Get(customerId);
+
+            if (customer != null) //adapt
+            {
+                return customer.ProjectedAs<CustomerDTO>();
+            }
+            else
+                return null;
+        }
+
+        public List<CountryDTO> FindCountries(int pageIndex, int pageCount)
+        {
+            if (pageIndex < 0 || pageCount <= 0)
+                throw new ArgumentException(_resources.GetStringResource(LocalizationKeys.Application.warning_InvalidArgumentsForFindCountries));
+
+            //recover countries
+            var countries = _countryRepository.GetPaged(pageIndex, pageCount, c => c.CountryName, false);
+
+            if (countries != null
+                &&
+                countries.Any())
+            {
+                return countries.ProjectedAsCollection<CountryDTO>();
+            }
+            else // no data.
+                return null;
+        }
+
+        public List<CountryDTO> FindCountries(string text)
+        {
+            //get the specification
+            ISpecification<Country> specification = CountrySpecifications.CountryFullText(text);
+
+            //Query this criteria
+            var countries = _countryRepository.AllMatching(specification);
+
+            if (countries != null
+                &&
+                countries.Any())
+            {
+                return countries.ProjectedAsCollection<CountryDTO>();
+            }
+            else // no data
+                return null;
         }
     }
 }
